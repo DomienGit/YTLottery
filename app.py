d3150d443ee52dc98fae7209bcde418597637d08
Domiendev <domien02311@gmail.com>
2026-03-03 17:17:04 +0100

    feat: funkcja wyciagajaca id video, funkcja wyswietlajaca komentarze z live

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from ytlottery!")
+import pytchat
+import re
 
+def get_video_id(url):
+     """
+     Wyciąga Video ID z różnych formatów linków YouTube.
+     Zwraca ID (11 znaków) lub None, jeśli link jest niepoprawny.
+     """
+     pattern = r'(?:v=|\/)([0-9A-Za-z_-]{11})(?:[&?]|$)'
+     match = re.search(pattern, url)
+
+     if match:
+         return match.group(1)
+     return None
+
+def main(video_url):
+    video_id = get_video_id(video_url)
+    if not video_id:
+        print("Invalid YouTube URL")
+        return
+
+    chat = pytchat.create(video_id=video_id)
+    while chat.is_alive():
+        for c in chat.get().sync_items():
+            print(f"{c.datetime} [{c.author.name}]- {c.message}")
 
 if __name__ == "__main__":
-    main()
+    print("Starting to collect comments...")
+    main("https://www.youtube.com/watch?v=ft0oRojsZAA")
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import streamlit as st
 
-st.title("Testowa aplikacja YTLottery")
+st.set_page_config(page_title="YT Lottery", page_icon=":tada:", layout="centered")
+st.title("YT Lottery")
 
-st.write("Witaj w prostej aplikacji do testowania Streamlit.")
+url = st.sidebar.text_input("Enter YouTube video URL:", placeholder="https://www.youtube.com/watch?v=VIDEO_ID_HERE")
+keyword = st.sidebar.text_input("Enter keyword to search for:", placeholder="e.g., 'win', 'prize', 'winner'")
 
-st.info("Jeśli to widzisz, oznacza to, że Streamlit jest poprawnie skonfigurowany.")
+col1, col2 = st.sidebar.columns(2)
+if col1.button("Start", type="primary"):
+    #logika zbierania komentarzy i sprawdzania, czy zawierają słowo kluczowe
+    st.write("Starting to collect comments...")
 
-if st.button("Pokaż magię!"):
-    st.balloons()
-    st.success("Magia! Przycisk działa.")
+if col2.button("Stop"):
+    #logika zatrzymywania zbierania komentarzy
+    st.write("Stopped collecting comments.")
+
+placeholder_list = st.empty()
+    
