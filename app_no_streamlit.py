cc35de46a568a23d57c32f81c7f20c02447dfe9e
domien <domien02311@gmail.com>
2026-03-12 19:23:11 +0100

    e

--- app_no_streamlit.py
+++ app_no_streamlit.py
@@ -17,7 +17,7 @@ def _get_video_id(url):
          return match.group(1)
      return None
 
-def start_chat_listener(video_url):
+def start_chat_listener(video_url, authors_set=None):
     video_id = _get_video_id(video_url)
     if not video_id:
         print("Nieprawidłowy link do filmu.")
@@ -26,14 +26,23 @@ def start_chat_listener(video_url):
     chat = pytchat.create(video_id=video_id)
     while not stop_event.is_set() and chat.is_alive():
         for c in chat.get().sync_items():
-            print(c.author.name)
+            if authors_set is not None and c.author.name not in authors_set:
+                authors_set.append(c.author.name)
+
 
 
 if __name__ == "__main__":
-    video_url = input("Podaj link do filmu YouTube: ")
-    threaded_chat_listener = multiprocessing.Process(target=start_chat_listener, args=(video_url,))
-    threaded_chat_listener.start()
-    ask = input("Naciśnij Enter, aby zatrzymać zbieranie komentarzy...")
-    stop_event.set()
-    threaded_chat_listener.join()
+    with multiprocessing.Manager() as manager:
+        authors_set = manager.list()  # Tworzymy zarządzany zbiór
+        video_url = input("Podaj link do filmu YouTube: ")
+        threaded_chat_listener = multiprocessing.Process(target=start_chat_listener, args=(video_url, authors_set))
+        threaded_chat_listener.start()
+        ask = input("Naciśnij Enter, aby zatrzymać zbieranie komentarzy...")
+        stop_event.set()
+        threaded_chat_listener.join()
+        if not authors_set:
+            print("Nie zebrano żadnych komentarzy.")
+        else:
+            for index, author in enumerate(list(authors_set)):
+                print(f"{index + 1}. {author}")
     print("Zbieranie komentarzy zostało zatrzymane.")
