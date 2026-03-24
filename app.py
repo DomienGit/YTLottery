c5cd51e23b5c880d7cf381a1bc52e76bc80a44dc
DomienGit <domien02311@gmail.com>
2026-03-24 16:53:53 +0100

    edit for render.com

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = '';
 
 // Elementy DOM
 const urlPanel = document.getElementById('urlPanel');
