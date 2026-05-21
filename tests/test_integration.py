42b3830db00ac342be828b6df9dca1d3c569abb5
domien <domien02311@gmail.com>
2026-05-21 18:19:54 +0200

    fead: update test_author_crud_flow, add deleting and clearing

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -19,6 +19,23 @@ def test_author_crud_flow(integration_client):
     assert len(data) == 2
     assert data[1]["author"] == "Tom"
 
+    response = integration_client.post("/delete", json={"name": "Bob"})
+    assert response.status_code == 200
+    assert response.json()["success"] == True
+
+    response = integration_client.get("/authors")
+    data = response.json()["message"]
+    assert len(data) == 1
+    assert data[0]["author"] == "Tom"
+
+    response = integration_client.post("/clear")
+    assert response.status_code == 200
+    assert response.json()["success"] == True
+
+    response = integration_client.get("/authors")
+    data = response.json()["message"]
+    assert len(data) == 0
+
 def test_draw_flow(integration_client):
     authors = ["Bob", "Tom", "Kliff"]
     [integration_client.post("/add-author", json={"name": author}) for author in authors]
@@ -26,3 +43,5 @@ def test_draw_flow(integration_client):
     response = integration_client.post("/draw")
     assert response.status_code == 200
     assert response.json()["winner"] in authors
+    assert response.json()["success"] == True
+    assert response.json()["message"] == "Winner drawn"
