81c82492d3a0c552c1e678477ff95f878aa26040
domien <domien02311@gmail.com>
2026-05-25 09:17:42 +0200

    feat: add GitHub Actions CI workflow and new integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,4 +1,3 @@
-from fastapi.testclient import TestClient
 
 def test_author_crud_flow(integration_client):
     response = integration_client.post("/add-author", json={"name": "Bob"})
@@ -46,5 +45,54 @@ def test_draw_flow(integration_client):
     assert response.json()["success"] == True
     assert response.json()["message"] == "Winner drawn"
 
-''' 3. Flow czyszczenia — dodaj autorów → POST /clear → sprawdź pustą listę → POST /draw powinien zwrócić błąd
-   #4. Pełny cykl — dodaj autorów → wylosuj → usuń zwyciężcę → dodaj nowego → wylosuj ponownie → wyczyść wszystko → draw powinien zwrócić błąd'''
+
+def test_clearing_flow(integration_client):
+    authors = ["Bob", "Tom", "Kliff"]
+    [integration_client.post("/add-author", json={"name": author}) for author in authors]
+
+    response = integration_client.post("/clear")
+    assert response.status_code == 200
+    assert response.json()["success"] == True
+
+    response = integration_client.get("/authors")
+    data = response.json()["message"]
+    assert len(data) == 0
+
+    response = integration_client.post("/draw")
+    assert response.status_code == 200
+    assert response.json()["success"] == False
+    assert response.json()["message"] == "No authors to draw from"
+
+def test_full_cycle(integration_client):
+    authors = ["Bob", "Tom", "Kliff"]
+    [integration_client.post("/add-author", json={"name": author}) for author in authors]
+
+    response = integration_client.post("/draw")
+    winner = response.json()["winner"]
+    assert response.status_code == 200
+    assert winner in authors
+    assert response.json()["success"] == True
+
+    response = integration_client.post("/delete", json={"name": winner})
+    assert response.status_code == 200
+    assert response.json()["success"] == True
+
+    new_author = "Alice"
+    response = integration_client.post("/add-author", json={"name": new_author})
+    assert response.status_code == 200
+    assert response.json()["success"] == True
+
+    response = integration_client.post("/draw")
+    new_winner = response.json()["winner"]
+    assert response.status_code == 200
+    assert new_winner in [author for author in authors if author != winner] + [new_author]
+    assert response.json()["success"] == True
+
+    response = integration_client.post("/clear")
+    assert response.status_code == 200
+    assert response.json()["success"] == True
+
+    response = integration_client.post("/draw")
+    assert response.status_code == 200
+    assert response.json()["success"] == False
+    assert response.json()["message"] == "No authors to draw from"
